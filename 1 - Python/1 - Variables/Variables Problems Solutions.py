0dff76b8afd7887dabddda191742cb41d95c2660
efekan <efekan>
2023-02-23 01:41:26 +0300

    organized folders

---
+++ 1 - Python/1 - Variables/Variables Problems Solutions.py
@@ -0,0 +1,24 @@
+# First Question Wrong , Reason Why Is That Will Make a Syntax Error
+
+# Question 2 :
+# -------------
+
+birth_year = 2003
+current_year = 2023
+
+print("Current Age : ",current_year-birth_year)
+
+# Question 3 :
+# -------------
+
+firstName = "Efekan"
+lastName = "Alipasha"
+
+print("Name : ",firstName,lastName)
+
+# Question 4 :
+# -------------
+
+# Answer is (1record, record-one, record^one, continue)
+
+
