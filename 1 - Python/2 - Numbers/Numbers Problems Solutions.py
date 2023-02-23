34c24ee4ba90441632e70409c1d6f26afa1d990d
efekan <efekan>
2023-02-23 09:49:51 +0300

    reorganized folder

---
+++ 1 - Python/2 - Numbers/Numbers Problems Solutions.py
@@ -0,0 +1,30 @@
+# Question 1 :
+# -------------
+
+area_field = 80 * 58
+print("Area of Football Field : ",area_field) #Ans: 4640
+
+# Question 2 :
+# -------------
+
+total_cost = 6 * 2.37
+cash_back = 20 - total_cost
+print("Cash Back : ",cash_back) #Ans: 5.779
+
+# Question 3 :
+# -------------
+
+length = 8
+area_of_Wall = length**2
+cost = area_of_Wall * 4.8
+print("Total Price : ",cost) #Ans: 307.2
+
+
+# Question 4 :
+# -------------
+
+num = 15
+print("Binary of Number 15 is : ",format(num,"b")) #Ans: 1111
+
+
+
