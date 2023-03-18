014b784dc91b94dd8284d1b52587d765cc363727
efekan <efekan>
2023-03-18 16:10:57 +0300

    organized folder

---
+++ 1 - Python/9 - Files/Question1 Solution.py
@@ -0,0 +1,21 @@
+word_stats = {}
+
+with open("poem.txt", "r") as f:
+    for line in f:
+        words = line.split(' ')
+        for word in words:
+            if word in word_stats:
+                word_stats[word] += 1
+            else:
+                word_stats[word] = 1
+
+print(word_stats)
+
+word_occurances = list(word_stats.values())
+max_count = max(word_occurances)
+print("Max Occurances of Any Word Is : ", max_count)
+
+print("Words With Max Occurances Are : ")
+for word, count in word_stats.items():
+    if count == max_count:
+        print(word)
---
+++ 1 - Python/9 - Files/Question2 Solution.py
@@ -0,0 +1,14 @@
+data = []
+
+for i in range(5):
+    line = input("Enter Sentence For Us : ")
+    data.append(line)
+
+with open("efile.txt", "w") as f:
+    for line in data:
+        f.write(line)
+        f.write("\n")
+
+with open("efile.txt", "r") as ff:
+    contents = ff.read()
+    print(contents)
