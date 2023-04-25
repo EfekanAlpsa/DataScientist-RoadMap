974340d49c5a0151d9b95eeabaffba48f22c9cbe
efekan <93012250+EfekanAlpsa@users.noreply.github.com>
2023-04-25 17:28:17 +0300

    update file for ML 3

--- 4 - Machine Learning/Machine Learning Tutorial Python 3 - Gradient Descent and Cost Function/gradient_descent.py
+++ 4 - Machine Learning/Machine Learning Tutorial Python 3 - Gradient Descent and Cost Function/gradient_descent.py
@@ -11,11 +11,12 @@ def gradient_descent(x, y):
 
     for i in range(iterations):
         y_predicted = m_curr * x + b_curr
-        md = -(2 / n) * sum(x * (y - y_predicted))
-        bd = -(2 / n) * sum(y - y_predicted)
+        cost = (1/n) * sum([val ** 2 for val in (y - y_predicted)])
+        md = -(2 / n) * sum(x * (y - y_predicted))  # Derivative of m
+        bd = -(2 / n) * sum(y - y_predicted)        # Derivative of b
         m_curr = m_curr - learning_rate * md
         b_curr = b_curr - learning_rate * bd
-        print("m {} , b {} , iteration {}".format(m_curr, b_curr, i))
+        print("m {} , b {} ,cost {},  iteration {}".format(m_curr, b_curr, cost, i))
 
 
 x = np.array([1, 2, 3, 4, 5])
